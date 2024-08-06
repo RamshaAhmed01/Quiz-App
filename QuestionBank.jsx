ca8636f5ee16772755aa3899c34f17aab77ab6fb
RamshaAhmed01 <159632282+RamshaAhmed01@users.noreply.github.com>
2024-08-06 21:18:50 +0500

    Add files via upload

---
+++ QuestionBank.jsx
@@ -0,0 +1,36 @@
+// QuestionBank.js
+
+const qBank = [
+    {
+        id: 1,
+        question: "What is the capital of Pakistan?",
+        options: ["Islamabad", "Peshawar", "Quetta", "Lahore"],
+        answer: "Islamabad",
+    },
+    {
+        id: 2,
+        question: "Which city is the capital of Punjab?",
+        options: ["Skkhur", "Faislabad", "Lahore", "Multan"],
+        answer: "Lahore",
+    },
+    {
+        id: 3,
+        question: "Which Province is the largest province of Pakistan?",
+        options: ["Sindh","Balochistan","Punjab","Khyber Pakhtoon Khua"],
+        answer: "Punjab"
+    },
+    {
+        id: 4,
+        question: "what is the total area of Pakistan?",
+        options: ["296396","256789","345789","456789"],
+        answer: "296396"
+    },
+    {
+        id: 5,
+        question: "Which city is known as the city of lights?",
+        options: ["Lahore","Karachi","Islamabad","Swat"],
+        answer: "Karachi"
+    },
+]
+
+export default qBank;
